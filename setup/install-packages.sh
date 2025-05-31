03f324c0f486d3e952d663b8a2e2e35b401e4a37
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-05-31 00:13:01 -0300

    install-packages and install-yay-packages refactoring

--- setup/install-packages.sh
+++ setup/install-packages.sh
@@ -2,21 +2,11 @@
 set -e
 
 PACKAGE_FILE="$(dirname "$(realpath "$0")")/../packages.txt"
-
-pacman -S --noconfirm --needed git
-git clone https://aur.archlinux.org/yay.git
-
-cd yay
-makepkg -sirc
-
-cd ..
-rm -rf yay
-
 packages=$(grep -vE '^\s*#|^\s*$' "$PACKAGE_FILE")
 
 echo "Installing the following packages:"
 echo "$packages"
 echo
 
-yay -S --noconfirm --needed $packages
-yay -Syu --noconfirm
+pacman -S --noconfirm --needed $packages
+pacman -Syu --noconfirm
