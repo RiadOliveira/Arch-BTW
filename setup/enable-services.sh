12cbe5d3d78c2af3b4d784e167247dbff4148481
Ríad Oliveira <riad.oliveira@hotmail.com>
2025-06-06 13:24:26 -0300

    User services removed from enable-services

--- setup/enable-services.sh
+++ setup/enable-services.sh
@@ -2,5 +2,3 @@
 set -e
 
 sudo systemctl enable sshd reflector.timer ufw systemd-networkd systemd-resolved iwd fstrim.timer tlp rtkit-daemon fail2ban haveged dbus-broker nvidia-suspend nvidia-hibernate nvidia-resume greetd
-
-systemctl --user --global enable pipewire pipewire-pulse wireplumber udiskie
