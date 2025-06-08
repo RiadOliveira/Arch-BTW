824640e1962d6ee09538f253950a2e32d76a19d6
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-08 20:24:19 -0300

    dunst added

--- config/usr;local;bin/set-power-profile.sh
+++ config/usr;local;bin/set-power-profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "true" ]; then
-  x86_energy_perf_policy power
+  sudo x86_energy_perf_policy power
 else
-  x86_energy_perf_policy performance
+  sudo x86_energy_perf_policy performance
 fi
