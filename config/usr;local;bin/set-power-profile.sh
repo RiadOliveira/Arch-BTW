6f62760952c10c27a5f8b0120b6555f38b79dc55
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-08 19:31:41 -0300

    cpupower removed from set-power-profile script

--- config/usr;local;bin/set-power-profile.sh
+++ config/usr;local;bin/set-power-profile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+
 if [ "$1" == "true" ]; then
-  cpupower frequency-set -g powersave
   x86_energy_perf_policy power
 else
-  cpupower frequency-set -g performance
   x86_energy_perf_policy performance
 fi
