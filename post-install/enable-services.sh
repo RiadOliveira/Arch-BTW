274c60192fbaf0fc70396936071574a23f0b94ac
RiadOliveira <riad.oliveira@hotmail.com>
2025-06-19 02:15:42 -0300

    Waybar added

--- post-install/enable-services.sh
+++ post-install/enable-services.sh
@@ -2,4 +2,4 @@
 
 sudo systemctl enable reflector.timer ufw fstrim.timer tlp rtkit-daemon fail2ban haveged greetd
 
-systemctl --user enable --now pipewire-pulse wireplumber udiskie gcr-ssh-agent.socket hyprpolkitagent
+systemctl --user enable --now pipewire-pulse wireplumber udiskie gcr-ssh-agent.socket hyprpolkitagent waybar
