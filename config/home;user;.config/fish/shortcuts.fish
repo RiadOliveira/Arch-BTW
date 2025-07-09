74164ad85a4776d215fb1dded777529e984bc194
RiadOliveira <riad.oliveira@hotmail.com>
2025-07-09 04:55:53 -0300

    check function improvment

--- config/home;user;.config/fish/shortcuts.fish
+++ config/home;user;.config/fish/shortcuts.fish
@@ -4,7 +4,7 @@ alias firefox='eval "$APP_CMD nohup firefox-developer-edition $DISOWN_CMD"'
 alias firefox-dev='eval "$APP_CMD nohup firefox-developer-edition $DISOWN_CMD"'
 alias firefox-developer-edition='eval "$APP_CMD nohup firefox-developer-edition $DISOWN_CMD"'
 
-alias code='eval "$APP_CMD code --password-store="gnome-libsecret" $ELECTRON_ARGS"'
+alias code='eval "$APP_CMD code . --password-store="gnome-libsecret" $ELECTRON_ARGS"'
 alias discord='eval "$APP_CMD nohup discord $ELECTRON_ARGS $DISOWN_CMD"'
 alias obsidian='eval "$APP_CMD nohup obsidian $ELECTRON_ARGS $DISOWN_CMD"'
 alias beekeeper-studio='eval "$APP_CMD nohup beekeeper-studio $ELECTRON_ARGS $DISOWN_CMD"'
@@ -36,7 +36,9 @@ alias preview-folders='fd -td | fzf --preview "eza -1 --color=always {}"'
 
 # Check
 function check
-  fd $argv -X bat
+  fd $argv | while read -l item
+    test -d $item; and eza $item; or bat $item
+  end
 end
 
 # Bathelp
