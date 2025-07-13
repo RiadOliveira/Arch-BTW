c758877b74c4a3047a943ab86138d0b93477bde0
RiadOliveira <riad.oliveira@hotmail.com>
2025-07-13 02:48:16 -0300

    Neovim initial config

---
+++ config/home;user;.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,39 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.o.number = true
+vim.o.confirm = true
+vim.o.expandtab = true
+vim.o.smartcase = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.scrolloff = 10
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" }
+  },
+  checker = { enabled = false },
+  rocks = { enabled = false },
+})
