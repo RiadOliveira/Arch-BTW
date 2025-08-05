6ed21d1ed8c7d8431ee1e5cbd797c4f56fef056a
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-04 23:19:01 -0300

    Git config updated for nvim

--- config/home;user;.config/nvim/lua/plugins/git.lua
+++ config/home;user;.config/nvim/lua/plugins/git.lua
@@ -5,6 +5,8 @@ return {
       "tpope/vim-fugitive",
     },
     config = function()
+      vim.keymap.set("n", "<leader>hd", ":Gvdiffsplit<CR>")
+
       require("gitsigns").setup({
         on_attach = function(bufnr)
           local gitsigns = require("gitsigns")
@@ -17,7 +19,6 @@ return {
 
           map("n", "<leader>hp", gitsigns.preview_hunk)
           map("n", "<leader>hi", gitsigns.preview_hunk_inline)
-          map("n", "<leader>hd", gitsigns.diffthis)
           map("n", "<leader>tw", gitsigns.toggle_word_diff)
 
           map("n", "]c", function()
