b623878c81cb4bd4aa2c7c63c5f20909052e07f3
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-05 02:38:25 -0300

    Dunst notifications customization

--- post-install/enable-services.sh
+++ post-install/enable-services.sh
@@ -2,4 +2,4 @@
 
 sudo systemctl enable reflector.timer ufw fstrim.timer tlp rtkit-daemon fail2ban haveged sddm
 
-systemctl --user enable --now pipewire-pulse wireplumber gcr-ssh-agent.socket hyprpolkitagent waybar docker.socket
+systemctl --user enable --now pipewire-pulse wireplumber gcr-ssh-agent.socket hyprpolkitagent waybar docker.socket battery-monitor.timer
