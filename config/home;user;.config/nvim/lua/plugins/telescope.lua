df9b637b6a1beac822491f8d06454ad5b24472f1
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-19 00:51:55 -0300

    Hidden files added to telescope

--- config/home;user;.config/nvim/lua/plugins/telescope.lua
+++ config/home;user;.config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,28 @@ return {
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local builtin = require("telescope.builtin")
+    require('telescope').setup({
+      defaults = {
+        hidden = true,
+        file_ignore_patterns = {
+          "node_modules/.*",
+          "%.git/.*",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          no_ignore = true
+        },
+        live_grep = {
+          additional_args = function()
+            return { "--hidden" }
+          end
+        },
+      }
+    })
 
+    local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<C-p>", builtin.find_files, {})
     vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
