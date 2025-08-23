48d53421ed23b9de9409383b0cf6cbb54f2687ff
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-23 01:10:41 -0300

    Apparmor moved to setup

--- post-install/enable-services.sh
+++ post-install/enable-services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo systemctl enable apparmor reflector.timer ufw fstrim.timer tlp rtkit-daemon fail2ban haveged sddm docker.socket
+sudo systemctl enable reflector.timer ufw fstrim.timer tlp rtkit-daemon fail2ban haveged sddm docker.socket
 
 systemctl --user enable --now pipewire-pulse wireplumber gcr-ssh-agent.socket waybar battery-monitor.timer
