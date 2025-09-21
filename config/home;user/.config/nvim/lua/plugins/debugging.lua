45b161292d7d4c5489447951999d5bf03612a5e3
RiadOliveira <riad.oliveira@hotmail.com>
2025-09-21 04:10:17 -0300

    Indent fixed for some configs

--- config/home;user/.config/nvim/lua/plugins/debugging.lua
+++ config/home;user/.config/nvim/lua/plugins/debugging.lua
@@ -1,111 +1,111 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-	},
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
-		local widgets = require("dap.ui.widgets")
-		dapui.setup()
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap, dapui = require("dap"), require("dapui")
+    local widgets = require("dap.ui.widgets")
+    dapui.setup()
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		vim.keymap.set("n", "<F5>", function()
-			dap.continue()
-		end)
-		vim.keymap.set("n", "<F10>", function()
-			dap.step_over()
-		end)
-		vim.keymap.set("n", "<F11>", function()
-			dap.step_into()
-		end)
-		vim.keymap.set("n", "<F12>", function()
-			dap.step_out()
-		end)
-		vim.keymap.set("n", "<leader>b", function()
-			dap.toggle_breakpoint()
-		end)
-		vim.keymap.set("n", "<leader>B", function()
-			dap.set_breakpoint()
-		end)
-		vim.keymap.set("n", "<leader>lp", function()
-			dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-		end)
-		vim.keymap.set("n", "<leader>dr", function()
-			dap.repl.open()
-		end)
-		vim.keymap.set("n", "<leader>dl", function()
-			dap.run_last()
-		end)
+    vim.keymap.set("n", "<F5>", function()
+      dap.continue()
+    end)
+    vim.keymap.set("n", "<F10>", function()
+      dap.step_over()
+    end)
+    vim.keymap.set("n", "<F11>", function()
+      dap.step_into()
+    end)
+    vim.keymap.set("n", "<F12>", function()
+      dap.step_out()
+    end)
+    vim.keymap.set("n", "<leader>b", function()
+      dap.toggle_breakpoint()
+    end)
+    vim.keymap.set("n", "<leader>B", function()
+      dap.set_breakpoint()
+    end)
+    vim.keymap.set("n", "<leader>lp", function()
+      dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+    end)
+    vim.keymap.set("n", "<leader>dr", function()
+      dap.repl.open()
+    end)
+    vim.keymap.set("n", "<leader>dl", function()
+      dap.run_last()
+    end)
 
-		vim.keymap.set({ "n", "v" }, "<leader>dh", function()
-			widgets.hover()
-		end)
-		vim.keymap.set({ "n", "v" }, "<leader>dp", function()
-			widgets.preview()
-		end)
-		vim.keymap.set("n", "<leader>df", function()
-			widgets.centered_float(widgets.frames)
-		end)
-		vim.keymap.set("n", "<leader>ds", function()
-			widgets.centered_float(widgets.scopes)
-		end)
+    vim.keymap.set({ "n", "v" }, "<leader>dh", function()
+      widgets.hover()
+    end)
+    vim.keymap.set({ "n", "v" }, "<leader>dp", function()
+      widgets.preview()
+    end)
+    vim.keymap.set("n", "<leader>df", function()
+      widgets.centered_float(widgets.frames)
+    end)
+    vim.keymap.set("n", "<leader>ds", function()
+      widgets.centered_float(widgets.scopes)
+    end)
 
-		dap.adapters.gdb = {
-			type = "executable",
-			command = "gdb",
-			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
-		}
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "gdb",
+      args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+    }
 
-		dap.configurations.c = {
-			{
-				name = "Launch",
-				type = "gdb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				args = {},
-				cwd = "${workspaceFolder}",
-				stopAtBeginningOfMainSubprogram = false,
-			},
-			{
-				name = "Select and attach to process",
-				type = "gdb",
-				request = "attach",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				pid = function()
-					local name = vim.fn.input("Executable name (filter): ")
-					return require("dap.utils").pick_process({ filter = name })
-				end,
-				cwd = "${workspaceFolder}",
-			},
-			{
-				name = "Attach to gdbserver :1234",
-				type = "gdb",
-				request = "attach",
-				target = "localhost:1234",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-			},
-		}
+    dap.configurations.c = {
+      {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        args = {},
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = false,
+      },
+      {
+        name = "Select and attach to process",
+        type = "gdb",
+        request = "attach",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        pid = function()
+          local name = vim.fn.input("Executable name (filter): ")
+          return require("dap.utils").pick_process({ filter = name })
+        end,
+        cwd = "${workspaceFolder}",
+      },
+      {
+        name = "Attach to gdbserver :1234",
+        type = "gdb",
+        request = "attach",
+        target = "localhost:1234",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+      },
+    }
 
-		dap.configurations.cpp = dap.configurations.c
-	end,
+    dap.configurations.cpp = dap.configurations.c
+  end,
 }
--- config/home;user/.config/nvim/lua/plugins/lsp-config.lua
+++ config/home;user/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,61 +1,61 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = { auto_install = true },
-	ensure_installed = {
-		"lua_ls",
-		"bashls",
-		"clangd",
-		"cmake",
-		"cssls",
-		"dockerls",
-		"html",
-		"jdtls",
-		"ts_ls",
-		"jsonls",
-		"marksman",
-		"nginx_language_server",
-		"prismals",
-		"pyright",
-		"docker_compose_language_service",
-		"tailwindcss",
-	},
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{
-			"mason-org/mason.nvim",
-			opts = {},
-		},
-		{
-			"neovim/nvim-lspconfig",
-			config = function()
-				vim.lsp.config("ts_ls", {
-					setup = {
-						on_attach = function(client)
-							client.server_capabilities.documentFormattingProvider = false
-							client.server_capabilities.documentRangeFormattingProvider = false
-						end,
-					},
-				})
+  "mason-org/mason-lspconfig.nvim",
+  opts = { auto_install = true },
+  ensure_installed = {
+    "lua_ls",
+    "bashls",
+    "clangd",
+    "cmake",
+    "cssls",
+    "dockerls",
+    "html",
+    "jdtls",
+    "ts_ls",
+    "jsonls",
+    "marksman",
+    "nginx_language_server",
+    "prismals",
+    "pyright",
+    "docker_compose_language_service",
+    "tailwindcss",
+  },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    {
+      "mason-org/mason.nvim",
+      opts = {},
+    },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        vim.lsp.config("ts_ls", {
+          setup = {
+            on_attach = function(client)
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end,
+          },
+        })
 
-				vim.lsp.config("jdtls", {
-					on_attach = function(client)
-						client.server_capabilities.semanticTokensProvider = false
-					end,
-				})
-			end,
-		},
-		{
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			opts = {
-				ensure_installed = {
-					"stylua",
-					"eslint_d",
-					"prettierd",
-					"google-java-format",
-				},
-				auto_update = true,
-				run_on_start = true,
-			},
-		},
-	},
+        vim.lsp.config("jdtls", {
+          on_attach = function(client)
+            client.server_capabilities.semanticTokensProvider = false
+          end,
+        })
+      end,
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      opts = {
+        ensure_installed = {
+          "stylua",
+          "eslint_d",
+          "prettierd",
+          "google-java-format",
+        },
+        auto_update = true,
+        run_on_start = true,
+      },
+    },
+  },
 }
