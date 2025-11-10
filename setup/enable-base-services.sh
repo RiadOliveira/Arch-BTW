3ce7880bf3a9d2adeba30c1699e3fa197a763645
RiadOliveira <riad.oliveira@hotmail.com>
2025-11-10 15:40:22 -0300

    sshd removed from base enabled services

--- setup/enable-base-services.sh
+++ setup/enable-base-services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo systemctl enable systemd-networkd systemd-resolved iwd sshd dbus-broker nvidia-suspend nvidia-hibernate nvidia-resume apparmor
+sudo systemctl enable systemd-networkd systemd-resolved iwd dbus-broker nvidia-suspend nvidia-hibernate nvidia-resume apparmor
