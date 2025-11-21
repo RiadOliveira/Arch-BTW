d4586a581bd7e27c36e97c374e5f20628f76f4a9
RiadOliveira <riad.oliveira@hotmail.com>
2025-11-21 02:37:23 -0300

    FZF style fixed

--- config/home;user/.config/fish/env.fish
+++ config/home;user/.config/fish/env.fish
@@ -6,7 +6,7 @@ set -U ELECTRON_ARGS '--enable-features=UseOzonePlatform,WaylandLinuxDrmSyncobj 
 
 # FZF DEFAULTS
 set -x FZF_DEFAULT_COMMAND 'fd'
-set -x FZF_DEFAULT_OPTS '--bind "alt-k:preview-page-up,alt-j:preview-page-down" --style minimal --color "border:#1064ad,prompt:#fc5ef0,pointer:#9c44fc,spinner:#9c44fc,info:#fc5ef0,marker:#fc5ef0,bg+:#2a2640"'
+set -x FZF_DEFAULT_OPTS '--bind "alt-k:preview-page-up,alt-j:preview-page-down" --style minimal --gutter " " --color "border:#1064ad,prompt:#fc5ef0,pointer:#9c44fc,spinner:#9c44fc,info:#fc5ef0,marker:#fc5ef0,bg+:#2a2640"'
 
 set -x FZF_CTRL_T_COMMAND 'fd -H'
 set -x FZF_CTRL_T_OPTS '--preview "if test -d {}; eza -1 --color=always {}; else; bat -n --color=always --line-range=:100 {}; end"'
