e701bb2e48e382ab4f9e104dd5ba6265c221c331
RiadOliveira <riad.oliveira@hotmail.com>
2026-03-12 02:09:58 -0300

    Bun added

--- config/home;user/.config/fish/shortcuts.fish
+++ config/home;user/.config/fish/shortcuts.fish
@@ -101,7 +101,7 @@ alias disk-check='df -h -x tmpfs -x devtmpfs -x squashfs'
 alias service-check='systemctl --failed'
 
 alias docker-clean='docker system prune -a --volumes && docker volume prune -a'
-alias cache-clean='rm -rf ~/.cache && pac-cache && pnpm cache delete && pnpm store prune && npm cache clean --force && clean'
+alias cache-clean='rm -rf ~/.cache && pnpm cache delete && pnpm store prune && npm cache clean --force && clean'
 
 alias cat='bat'
 alias man='batman'
