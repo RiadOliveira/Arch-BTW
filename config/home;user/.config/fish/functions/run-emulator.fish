fefb56902e5ade6acb6518d9981fa8638eb4c1f0
RiadOliveira <riad.oliveira@hotmail.com>
2026-03-27 04:14:37 -0300

    Run emulator funcion improvements

--- config/home;user/.config/fish/functions/run-emulator.fish
+++ config/home;user/.config/fish/functions/run-emulator.fish
@@ -1,3 +1,9 @@
 function run-emulator
-  uwsm app -- nohup $ANDROID_HOME/emulator/emulator -avd $argv[1] > /dev/null 2>&1 & disown
+  set AVD ($ANDROID_HOME/emulator/emulator -list-avds | head -n 1)
+
+  if test -n "$AVD"
+    uwsm app -- nohup $ANDROID_HOME/emulator/emulator -avd "$AVD" -gpu host -no-snapshot -no-boot-anim > /dev/null 2>&1 & disown
+  else
+    echo "No AVD found."
+  end
 end
