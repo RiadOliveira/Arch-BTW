01d052983e3570e03e1399ba3c3f08163f056f16
RiadOliveira <riad.oliveira@hotmail.com>
2026-04-08 21:48:41 -0300

    Nvim colorizer applied to all files

--- config/home;user/.config/nvim/lua/plugins/colorizer.lua
+++ config/home;user/.config/nvim/lua/plugins/colorizer.lua
@@ -1,12 +1,6 @@
 return {
   "norcalli/nvim-colorizer.lua",
   config = function()
-    require("colorizer").setup({
-      "css",
-      "jsx",
-      "tsx",
-      "html",
-      "conf",
-    })
+    require("colorizer").setup()
   end,
 }
