5428be3cfb8ae6e41360130f20043d74d41d6c00
RiadOliveira <riad.oliveira@hotmail.com>
2026-04-20 23:35:09 -0300

    Monitor modes and switching handling added

---
+++ config/home;user/.config/fish/completions/switch-monitor-mode.fish
@@ -0,0 +1,4 @@
+complete -c switch-monitor-mode -n "__fish_use_subcommand" -f -a "internal"
+complete -c switch-monitor-mode -n "__fish_use_subcommand" -f -a "external"
+complete -c switch-monitor-mode -n "__fish_use_subcommand" -f -a "extended"
+complete -c switch-monitor-mode -n "__fish_use_subcommand" -f -a "mirror"
